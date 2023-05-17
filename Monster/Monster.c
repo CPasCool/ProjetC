8efa5a6dfc85a0f3b177f4f9d13f9e159bef18c9
Edouard Hoest <edouard_hoest@ens.univ-artois.fr>
2023-05-17 08:29:22 +0200

    correct monsters

--- Monster/Monster.c
+++ Monster/Monster.c
@@ -1,18 +1,42 @@
-#include <stdio.h>
-#include "Monster\Monster.h"
+#include <stdlib.h>
+#include "Monster.h"
+
 typedef struct monster_{
-    char name;
+    char* name;
     int hp;
-    int strenght;
+    int strength;
     int shield;
 }monster;
 
-monster createNewMonster(char name, int hp, int strenght, int shield)
-{
-    monster newMonster = malloc(sizeof(newMonster));
+monster* createNewMonster(char* name, int hp, int strength, int shield){
+    monster *newMonster = (monster*) malloc(sizeof(struct monster_));
     newMonster->name = name;
     newMonster->hp = hp;
-    newMonster->strenght = strenght;
+    newMonster->strength = strength;
     newMonster->shield = shield;
     return newMonster;
+}
+
+monster* setMonster(monster* monster, char* name, int hp, int strength, int shield){
+    monster->name = name;
+    monster->hp = hp;
+    monster->strength = strength;
+    monster->shield = shield;
+    return monster;
+}
+
+char* getName(monster* monster){
+    return monster->name;
+}
+
+int getHealth(monster* monster){
+    return monster->hp;
+}
+
+int getStrength(monster* monster){
+    return monster->strength;
+}
+
+int getShield(monster* monster){
+    return monster->shield;
 }
--- Monster/Monster.h
+++ Monster/Monster.h
@@ -1,1 +1,6 @@
+#ifndef PROJETC_MONSTER_H
+#define PROJETC_MONSTER_H
+
 typedef struct monster_ monster;
+monster* createNewMonster(char* name, int hp, int strenght, int shield);
+#endif //PROJETC_MONSTER_H
