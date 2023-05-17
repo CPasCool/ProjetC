9c0b3ebbcc69670b586799b893a061ac1a990202
Benoit Fardoux <fardouxbenoit@gmail.com>
2023-05-17 08:53:01 +0200

    #3 : add file for catch input

---
+++ Keyboard/Keyboard.c
@@ -0,0 +1,13 @@
+//
+// Created by benfa on 17/05/2023.
+//
+
+#include "Keyboard.h"
+#include <conio.h>
+#include "stdio.h"
+
+char catchInput(){
+    int letter = getch();
+    printf("%d",letter);
+    return 'a';
+}
---
+++ Keyboard/Keyboard.h
@@ -0,0 +1,8 @@
+//
+// Created by benfa on 17/05/2023.
+//
+
+#ifndef PROJETC_KEYBOARD_H
+#define PROJETC_KEYBOARD_H
+char catchInput();
+#endif //PROJETC_KEYBOARD_H
