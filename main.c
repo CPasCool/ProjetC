48af101caa9757f02cf137699b2e50edbfe86fdc
Benoit Fardoux <fardouxbenoit@gmail.com>
2023-05-31 11:37:19 +0200

    FUNCTIONALL WINDOW

--- main.c
+++ main.c
@@ -1,15 +1,36 @@
+#include <stdio.h>
 #include "Tests/Character/TestCharacter.h"
 #include "Tests/Monsters/MonstersTests.h"
 #include "include/src/Game.h"
+#include "include/SDL2/SDL.h"
 
-
-int main() {
+int main(int argc, char *argv[]) {
     // TESTS
     testCharacter();
     testMonster();
-
+    printf("test");
+    SDL_Window *window = NULL;
+    if(0 != SDL_Init(SDL_INIT_VIDEO))
+    {
+        fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError());
+        return -1;
+    }
+    window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              500, 500, SDL_WINDOW_SHOWN);
+    if(NULL == window)
+        fprintf(stderr, "Erreur de creation de la fenetre : %s\n", SDL_GetError());
+    else
+    {
+            while (1){
+                printf("test");
+                printf("test2");
+            }
+//        SDL_Delay(500);
+    }
+//    SDL_Quit();
+    return 0;
     // Start the game
-    LaunchGame();
+//    LaunchGame();
     //    generateBoard();
     return 0;
 }
--- makefile
+++ makefile
@@ -1,12 +1,12 @@
 CC = gcc                  # Variable représentant le nom du compilateur.
 CFLAGS = -Wall -I include # Variable représentant les options de compilation.
-LDFLAGS = -L lib -lZDS    # Variable représentant les options d’édition de liens.
+LDFLAGS = -L lib -lmingw32 -lSDL2main -lSDL2 -mwindows    # Variable représentant les options d’édition de liens.
 
 
 SRC_C = $(wildcard *.c */*/*.c)
 
 main: $(SRC_C)
-	$(CC) -o $@ $^ $(CFLAGS) $(LIBS)
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
 
 clean :
 	rm -rf *.o
